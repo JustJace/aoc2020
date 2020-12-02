6894b19c358f284a19039d1b1d06f70fb8c451af
Jace Brewster <jacebrewster@gmail.com>
2020-12-02 13:02:09 -0600

    add tree structure, dfs, bfs, dijkstras

---
+++ PriorityQueue.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+public class PriorityQueue<T>
+{
+    private LinkedList<T> _nodes = new LinkedList<T>();
+    private readonly Func<T, int> _valueFn;
+
+    public PriorityQueue(Func<T, int> valueFn)
+    {
+        this._valueFn = valueFn;
+    }
+
+    public int Count => _nodes.Count;
+    
+    public void Enqueue(T data)
+    {
+        var newNode = new LinkedListNode<T>(data);
+        if (_nodes.Count == 0)
+        {
+            _nodes.AddFirst(newNode);
+        }
+        else
+        {
+            var current = _nodes.First;
+            while (current.Next != null)
+            {
+                if (_valueFn(current.Value) < _valueFn(data))
+                {
+                    _nodes.AddBefore(current, newNode);
+                    return;
+                }
+            }
+
+            _nodes.AddLast(newNode);
+        }
+    }
+
+    public T Dequeue()
+    {
+        var best = _nodes.First;
+        _nodes.Remove(best);
+        return best.Value;
+    }
+}
