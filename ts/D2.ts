a564f7bb9422be0ca85ed5855cc9d2180933adc7
Jace Brewster <jacebrewster@gmail.com>
2021-01-17 20:05:17 -0600

    D2 typescript

---
+++ ts/D2.ts
@@ -0,0 +1,59 @@
+import { readFileSync } from 'fs';
+import { timeAndPrint } from './time-and-print';
+
+class policy {
+    low: number;
+    high: number;
+    rule: string;
+    password: string;
+}
+
+function parseInput(): policy[] {
+    const policies: policy[] = [];
+    const content = <string>readFileSync('../inputs/D2.input','utf8');
+    for (let line of content.split("\n")) {
+        const match = line.match(/([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)/)
+        policies.push(<policy>{
+            low: +match[1],
+            high: +match[2],
+            rule: match[3],
+            password: match[4]
+        });
+    }
+    return policies;
+}
+
+function p1() {
+    const policies = parseInput();
+    let valid = 0;
+
+    for (let policy of policies) {
+        let count = 0;
+
+        for (let char of policy.password) {
+            if (char == policy.rule) count++;
+            if (count > policy.high) break;
+        }
+
+        if (count >= policy.low && count <= policy.high)
+            valid++;
+    }
+
+    return valid;
+}
+
+function p2() {
+    const policies = parseInput();
+    let valid = 0;
+
+    for (let policy of policies) {
+        if ((policy.password[policy.low - 1] == policy.rule) 
+        !== (policy.password[policy.high - 1] == policy.rule))
+            valid ++;
+    }
+
+    return valid;
+}
+
+timeAndPrint(p1);
+timeAndPrint(p2);
